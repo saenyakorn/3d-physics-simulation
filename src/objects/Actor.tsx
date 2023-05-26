5fe19b14dbfb95e7481fabe0e872b136b497977b
Nut Pinyo <bomb.np@gmail.com>
2023-05-26 23:35:23 +0700

    use standard material

--- src/objects/Actor.tsx
+++ src/objects/Actor.tsx
@@ -1,15 +1,14 @@
 import { forwardRef } from 'react'
 
-import { Box } from '@react-three/drei'
-
 import { Mesh } from 'three'
 
 interface ActorProps {}
 
 export const Actor = forwardRef<Mesh, ActorProps>(function Actor({ ...props }, ref) {
   return (
-    <mesh ref={ref}>
-      <Box castShadow receiveShadow material-color="#EAE0AD" />
+    <mesh ref={ref} castShadow receiveShadow>
+      <boxGeometry />
+      <meshStandardMaterial color="yellow" />
     </mesh>
   )
 })
